637e61638ab269fb041f758427f12f3bba6257b6
swordgeo <swordgeo1094@yahoo.com>
2023-03-27 17:35:18 -0400

    expand to 180 days of data

--- src/data-rips/data-rip.mjs
+++ src/data-rips/data-rip.mjs
@@ -7,16 +7,16 @@ import { DateTime } from 'luxon';
 
 
 const deckListFileName = 'src/json/deck_data_sample.json';
-const daysToKeep = 180; 
+const daysToKeep = 181; 
 
 function filterOldDecks(decks) {
   const oldestDateToKeep = DateTime.local().minus({ days: daysToKeep });
   return decks.filter(dayDecks => {
     if (!dayDecks || dayDecks.length === 0) {
       // If this day's decks array is empty or does not exist, discard it
-      return false;
+      return false; 
     }
-    // Get the date of the first deck in this day's list
+    // Get the date of the first deck in this day's list 
     // console.log(dayDecks[0]);
     const dateStr = dayDecks[0].date_creation;
     const date = DateTime.fromISO(dateStr);
@@ -27,7 +27,7 @@ function filterOldDecks(decks) {
 
 
 async function ripDeckData(outfileName) { 
-  const daysToFetch = 90;// Check if data file exists
+  const daysToFetch = 180;// Check if data file exists
 
   let start_date;
   if (fs.existsSync(outfileName)) {
@@ -36,7 +36,7 @@ async function ripDeckData(outfileName) {
       const lastDate = DateTime.fromISO(data[data.length - 1][0].date_creation);
       start_date = lastDate.plus({ days: 1 });
     } else {
-      start_date = DateTime.local().minus({ days: daysToFetch - 1 });
+      start_date = DateTime.local().minus({ days: daysToFetch});
     }
   }
 
@@ -44,12 +44,14 @@ async function ripDeckData(outfileName) {
   const month_data = [];
 
   // Iterate over each day in the range
-  for (let day = 0; day < daysToFetch; day++) {
+  // we're setting things to day = 1 and <= so that it skips the current day
+  // Which we don't want to prematurely pull data for until that day is finished
+  for (let day = 1; day <= daysToFetch; day++) {
     const current_date = DateTime.local().minus({ days: day });
-    const dateString = current_date.toISODate();
+    const dateString = current_date.toISODate(); 
 
     // Check if data for this day already exists in the file
-    if (fs.existsSync(outfileName)) {
+    if (fs.existsSync(outfileName)) { 
       const data = JSON.parse(fs.readFileSync(outfileName));
       if (data.some(day => day[0].date_creation.startsWith(dateString))) {
         // console.log(`Data for ${dateString} already exists, skipping...`);
@@ -73,132 +75,10 @@ async function ripDeckData(outfileName) {
   // Reverse the order of the daily data
   month_data.reverse();
 
-  // Write the month's data to a JSON file
-  // fs.writeFileSync(outfileName, JSON.stringify(month_data));
+
 
   return month_data;
 }
-
-// async function ripDeckData(outfileName, daysToFetch = 5) {
-//   // Check if data file exists
-//   let start_date;
-//   let data = [];
-//   if (fs.existsSync(outfileName)) {
-//     data = JSON.parse(fs.readFileSync(outfileName));
-//     if (data.length > 0) {
-//       const lastDate = DateTime.fromISO(data[data.length - 1][0].date_creation);
-//       start_date = lastDate.plus({ days: 1 });
-//     } else {
-//       start_date = DateTime.local().minus({ days: daysToFetch - 1 });
-//     }
-//   } else {
-//     start_date = DateTime.local().minus({ days: daysToFetch - 1 });
-//   }
-
-//   // Define list to store daily data
-//   const month_data = [];
-
-//   // Iterate over each day in the range
-//   for (let day = 0; day < daysToFetch; day++) {
-//     const current_date = start_date.plus({ days: day });
-
-//     // Check if we already have data for this date
-//     const existingData = data.find((d) => {
-//       const firstDeckDate = DateTime.fromISO(d[0].date_creation);
-//       return firstDeckDate.toISODate() === current_date.toISODate();
-//     });
-
-//     if (existingData) {
-//       console.log(`Data for ${current_date.toISODate()} already exists, skipping...`);
-//       continue; // skip this day and move on to the next day
-//     }
-
-//     const url = `https://marvelcdb.com/api/public/decklists/by_date/${current_date.toISODate()}`;
-//     const response = await fetch(url);
-//     const responseData = await response.json();
-//     if ('error' in responseData) {
-//       console.log(`Error fetching data for ${current_date.toISODate()}: ${responseData.error.message}`);
-//       continue; // skip this day and move on to the next day
-//     }
-//     month_data.push(responseData);
-//   }
-
-//   // Write the month's data to a JSON file
-//   // fs.writeFileSync(outfileName, JSON.stringify(month_data));
-
-//   return month_data;
-// }
-
-
-
-
-
-
-// async function ripDeckData(outfileName) {
-//   const daysToFetch = 2;
-
-//   // Define list to store daily data
-//   const month_data = [];
-
-//   // Iterate over each day in the range
-//   for (let day = 1; day <= daysToFetch; day++) {
-//     const current_date = DateTime.local().minus({ days: day });
-//     const url = `https://marvelcdb.com/api/public/decklists/by_date/${current_date.toISODate()}`;
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     if ('error' in data) {
-//       console.log(`Error fetching data for ${current_date.toISODate()}: ${data.error.message}`);
-//       continue; // skip this day and move on to the next day
-//     }
-//     month_data.push(data);
-//   }
-
-//     // Reverse the order of the array
-//     month_data.reverse();
-//   // Write the month's data to a JSON file
-//   // fs.writeFileSync(outfileName, JSON.stringify(month_data));
-
-//   return month_data;
-// }
-
-// async function ripDeckData(outfileName) {
-//   const daysToFetch = 5;
-
-//   // Check if data file exists
-//   let start_date;
-//   if (fs.existsSync(outfileName)) {
-//     const data = JSON.parse(fs.readFileSync(outfileName));
-//     if (data[data.length - 1].length > 0) {
-//       const lastDate = DateTime.fromISO(data[data.length - 1][0].date_creation);
-//       start_date = lastDate.plus({ days: 1 });
-//     } else {
-//       start_date = DateTime.local().minus({ days: daysToFetch - 1 });
-//     }
-//   }
-
-//   // Define list to store daily data
-//   const month_data = [];
-
-//   // Iterate over each day in the range
-//   for (let day = 0; day < daysToFetch; day++) {
-//     const current_date = start_date.plus({ days: day });
-//     const url = `https://marvelcdb.com/api/public/decklists/by_date/${current_date.toISODate()}`;
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     if ('error' in data) {
-//       console.log(`Error fetching data for ${current_date.toISODate()}: ${data.error.message}`);
-//       continue; // skip this day and move on to the next day
-//     }
-//     month_data.push(data);
-//   }
-
-//   // Write the month's data to a JSON file
-//   // fs.writeFileSync(outfileName, JSON.stringify(month_data));
-
-//   return month_data;
-// }
-
-
 
 export async function updateDeckData() {
   // Read the existing deck data from the file
